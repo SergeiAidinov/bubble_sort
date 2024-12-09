50b5b31bdc9b629f18322aee68691cabb5a9e83c
Sergei Aidinov <incoming34@yandex.ru>
2024-12-10 00:45:03 +0300

    final version

--- main.c
+++ main.c
@@ -6,18 +6,27 @@ void show_array(int array[], int size);
 
 int main(void) {
     signed int init_array[] = {3, 2, 1};
+    printf("Original array: ");
+    show_array(init_array, 3);
     signed int *sorted_array = sort(init_array, 3);
+    printf("Sorted array: ");
     show_array(sorted_array, 3);
     signed int init_array_1[] = {1024, -192, 0, 576, 12, 12, -11};
-    show_array(sort(init_array_1, 7), 7);
+    printf("Original array: ");
+    show_array(init_array_1, 7);
+    signed int *sorted_array_1 = sort(init_array_1, 7);
+    printf("Sorted array: ");
+    show_array(sorted_array_1, 7);
     return 0;
 }
 
 signed int *sort(int array[], int size) {
-    printf("\nStart sorting...\n");
+    printf("Start sorting... ");
     signed int array_changed = 1;
     signed int temp_int;
+    int iterations = 0;
     while (array_changed) {
+        iterations++;
         array_changed = 0;
         for (int i = 0; i < size - 1; i++) {
             signed int first_arg = array[i];
@@ -30,6 +39,7 @@ signed int *sort(int array[], int size) {
             }
         }
     }
+    printf("Sorting finished in %d iterations.\n", iterations);
     return array;
 }
 
@@ -37,4 +47,5 @@ void show_array(int array[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", array[i]);
     }
+    printf("\n");
 }
